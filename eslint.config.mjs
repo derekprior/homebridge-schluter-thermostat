a79f92e659cbb67ddece761c69a663f3e24282f6
Derek Prior <derekprior@gmail.com>
2024-10-17 21:32:53 -0400

    Migrate to eslint flat configuration
    This is required by newer versions of eslint.

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["**/dist"],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ),
+  {
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2018,
+      sourceType: "module",
+    },
+
+    rules: {
+      semi: ["off"],
+      "dot-notation": "off",
+      eqeqeq: "warn",
+      curly: ["warn", "all"],
+      "prefer-arrow-callback": ["warn"],
+      "no-console": ["warn"],
+      "no-non-null-assertion": ["off"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
+];
